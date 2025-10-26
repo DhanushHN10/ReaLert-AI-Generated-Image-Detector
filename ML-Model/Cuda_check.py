5aa953f9c5cb665dcb13d9eb700e49e6021f89bf
DhanushHN10 <dhanush.hn01@gmail.com>
2025-10-26 11:28:50 +0530

    Script to check if cuda is recognized and working

---
+++ ML-Model/Cuda_check.py
@@ -0,0 +1,7 @@
+import torch
+print("PyTorch CUDA version:", torch.version.cuda)
+print("CUDA available:", torch.cuda.is_available())
+if torch.cuda.is_available():
+    print("Device count:", torch.cuda.device_count())
+    print("Current device:", torch.cuda.current_device())
+    print("Device name:", torch.cuda.get_device_name(torch.cuda.current_device()))
